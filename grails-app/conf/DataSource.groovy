da45804dc7bef2ae24fc0c30af298b88559c3e28
jean-michel guillaume <guillauj@gmail.com>
2013-12-15 14:43:18 +0100

    datasource and favicon.ico

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,52 +1,52 @@
-dataSource {
-    pooled = true
-    driverClassName = "org.h2.Driver"
-    username = "sa"
-    password = ""
-}
-openShift{
-    pooled = true
-    driverClassName = "com.mysql.jdbc.Driver"
-    dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
-    username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME")
-    password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD")
-}
 hibernate {
-    cache.use_second_level_cache = true
-    cache.use_query_cache = false
-    cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
-//    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
+  cache.use_second_level_cache = true
+  cache.use_query_cache = false
+  cache.region.factory_class = 'net.sf.ehcache.hibernate.EhCacheRegionFactory' // Hibernate 3
+  //    cache.region.factory_class = 'org.hibernate.cache.ehcache.EhCacheRegionFactory' // Hibernate 4
 }
 
 // environment specific settings
 environments {
-    development {
-        dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
+  development {
+    dataSource {
+      pooled = true
+      driverClassName = "org.h2.Driver"
+      username = "sa"
+      password = ""
+      dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
+      url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
     }
-    test {
-        dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
-        }
+  }
+  test {
+    dataSource {
+      pooled = true
+      driverClassName = "org.h2.Driver"
+      username = "sa"
+      password = ""
+      dbCreate = "update"
+      url = "jdbc:h2:mem:testDb;MVCC=TRUE;LOCK_TIMEOUT=10000"
     }
-    production {
-        openShift {
-            dbCreate = "create-drop"
-            url = "jdbc:mysql://${System.getenv('OPENSHIFT_MYSQL_DB_HOST')}:${System.getenv('OPENSHIFT_MYSQL_DB_PORT')}/ecouteentretien?useUnicode=yes&characterEncoding=UTF-8"
-            properties {
-               maxActive = -1
-               minEvictableIdleTimeMillis=1800000
-               timeBetweenEvictionRunsMillis=1800000
-               numTestsPerEvictionRun=3
-               testOnBorrow=true
-               testWhileIdle=true
-               testOnReturn=false
-               validationQuery="SELECT 1"
-               jdbcInterceptors="ConnectionState"
-            }
-        }
+  }
+  production {
+    dataSource {
+      pooled = true
+      driverClassName = "com.mysql.jdbc.Driver"
+      dialect = "org.hibernate.dialect.MySQL5InnoDBDialect"
+      username = System.getenv("OPENSHIFT_MYSQL_DB_USERNAME")
+      password = System.getenv("OPENSHIFT_MYSQL_DB_PASSWORD")
+      dbCreate = "update"
+      url = "jdbc:mysql://${System.getenv('OPENSHIFT_MYSQL_DB_HOST')}:${System.getenv('OPENSHIFT_MYSQL_DB_PORT')}/ecouteentretien?useUnicode=yes&characterEncoding=UTF-8"
+      //            properties {
+      //               maxActive = -1
+      //               minEvictableIdleTimeMillis=1800000
+      //               timeBetweenEvictionRunsMillis=1800000
+      //               numTestsPerEvictionRun=3
+      //               testOnBorrow=true
+      //               testWhileIdle=true
+      //               testOnReturn=false
+      //               validationQuery="SELECT 1"
+      //               jdbcInterceptors="ConnectionState"
+      //            }
     }
+  }
 }
